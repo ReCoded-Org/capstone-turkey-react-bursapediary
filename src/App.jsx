a992eeb8012575f593dbbf3a447db23474095e5f
orkunseyman <orkunseyman@gmail.com>
2022-04-10 14:28:06 +0300

    rename signin-signup pages name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignInPage';
+import SignUp from './pages/SignUpPage';
 
 function App() {
   return (
