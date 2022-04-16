ac0ce7051f2f2538adb13dd2c786a4bd45c77a31
Orkun Seyman <orkunseyman@192.168.1.10>
2022-04-16 14:28:57 +0300

    fix: fix responsive issue

--- src/components/AuthenticationModuleSpan/AuthenticationModuleSpan.jsx
+++ src/components/AuthenticationModuleSpan/AuthenticationModuleSpan.jsx
@@ -9,7 +9,7 @@ function AuthenticationModuleSpan() {
   return (
     <div className="hidden sm:flex flex-col justify-evenly grid-rows-5  col-span-3 w-full rectangle bg-[#FEEEEE]">
       <div className="pl-12">
-        <h1 className="text-[#6A2C70]  pt-16 mr-12   text-5xl font-bold text-center sm:text-left">
+        <h1 className="text-[#6A2C70]  pt-16 mr-12 text-5xl font-bold text-center sm:text-left">
           Dreams Will <p className="break-words" /> Come True
         </h1>
         <p className="text-[#6A2C70] pt-6 mr-2 mb-16 mr-12 text-center sm:text-left">
@@ -18,7 +18,7 @@ function AuthenticationModuleSpan() {
           dreamers and help their dreams to come true.
         </p>
       </div>
-      <div className="ml-12 flex flex-wrap gap-10">
+      <div className="ml-12 mr-14 flex flex-wrap gap-10">
         <a
           href="https://twitter.com/"
           target="_blank"
