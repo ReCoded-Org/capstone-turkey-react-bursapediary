9a8a7d382b9ae1be7ee975b5e35fd801c743e928
Akin <akinkavak1@gmail.com>
2022-04-19 23:54:07 +0300

    fix: fix package-lock file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Home from './pages/Home';
 import Footer from './components/Footer/Footer';
-import ContactUs from './pages/ContactUsPage/ContactUs';
 
 function App() {
   return (
     <div>
-      <ContactUs />
       <Home />
       <Footer />
     </div>
