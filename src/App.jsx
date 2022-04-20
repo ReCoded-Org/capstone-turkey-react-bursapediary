d60e9fa0002c6dad4283edf1a412b08647ffc454
iseluin <kutay.kagan.ozenn@gmail.com>
2022-04-20 23:12:55 +0300

    fix:remove faq from render method

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import FaqPage from './pages/FAQ/FaqPage';
 import './App.css';
 import Home from './pages/Home';
 import Footer from './components/Footer/Footer';
@@ -6,7 +5,6 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div>
-      <FaqPage />
       <Home />
       <Footer />
     </div>
