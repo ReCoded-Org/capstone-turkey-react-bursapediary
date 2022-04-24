5364534345d188a2dfc71269c12395e5fe202365
orkunseyman <orkunseyman@gmail.com>
2022-04-24 20:36:58 +0300

    fix: remove home navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar/Navbar';
 import Home from './pages/Home';
@@ -10,15 +9,11 @@ import SignInPage from './pages/SignInPage/SignInPage';
 import { SIGN_UP_ROUTE, SIGN_IN_ROUTE, HOME_ROUTE } from './routes';
 
 function App() {
-  const user = useSelector((state) => state.user.currentUser);
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route
-          path={HOME_ROUTE}
-          element={user ? <Home /> : <Navigate to={SIGN_IN_ROUTE} />}
-        />
+        <Route path={HOME_ROUTE} element={<Home />} />
         <Route path={SIGN_UP_ROUTE} element={<SignUpPage />} />
         <Route path={SIGN_IN_ROUTE} element={<SignInPage />} />
       </Routes>
