ecef70e21f9a1cee369621dca20ccb6f710a7e3a
OzerOzturk <ozerozturk1636@gmail.com>
2022-04-26 23:29:54 +0300

    create folders and add images.

---
+++ src/components/AboutUs/TeamCard.jsx
@@ -0,0 +1,9 @@
+function TeamCard() {
+  return (
+    <div className="mb-5 mt-5">
+      <h1>deneme teamcard</h1>
+    </div>
+  );
+}
+
+export default TeamCard;
---
+++ src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,9 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>about us</h1>
+    </div>
+  );
+}
+
+export default AboutUs;
