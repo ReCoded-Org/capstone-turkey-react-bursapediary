fb8fbd980e4c2cb6dc325a2cf9c78002d5e9327a
OzerOzturk <ozerozturk1636@gmail.com>
2022-04-28 00:59:22 +0300

    add structure of the about us page.

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,34 @@
+import TeamCard from '../../components/AboutUs/TeamCard';
+
 function AboutUs() {
   return (
     <div>
-      <h1>about us</h1>
+      <section>
+        <img
+          src="https://user-images.githubusercontent.com/60944453/162048684-bbbe43f3-cd5b-4e93-908f-825ac7166ed1.PNG"
+          alt=""
+        />
+        <div>
+          <h1>About Us</h1>
+          <p>
+            We are a team of builders helping people reach their dreams while
+            others support them.
+          </p>
+        </div>
+      </section>
+      <section>
+        <h1>Our Story</h1>
+        <p>
+          In the React Web Development Bootcamp offered by Re:Coded, we decided
+          to build something meaninful in a way that it makes impact in lives of
+          people.
+        </p>
+      </section>
+      <section>
+        <h1>Our Team</h1>
+        <p>Meet the people behind our great product.</p>
+        <TeamCard />
+      </section>
     </div>
   );
 }
