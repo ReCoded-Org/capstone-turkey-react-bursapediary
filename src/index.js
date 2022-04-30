1899750a9ab37006cb82edf2aa07b689e74fe556
bayogluteoman <bayogluteoman@gmail.com>
2022-04-30 18:28:18 +0300

    fix: fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
